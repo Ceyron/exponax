7837f1341a3044cef088fa62f0d5dc9aaab044fc
Felix Koehler <f.koehler@tum.de>
2024-03-13 10:27:46 +0100

    Add a clamping IC generator

--- exponax/ic/__init__.py
+++ exponax/ic/__init__.py
@@ -1,4 +1,5 @@
 from ._base_ic import BaseIC, BaseRandomICGenerator
+from ._clamping import ClampingICGenerator
 from ._diffused_noise import DiffusedNoise
 from ._gaussian_random_field import GaussianRandomField
 from ._multi_channel import MultiChannelIC, RandomMultiChannelICGenerator
@@ -7,6 +8,7 @@ from ._truncated_fourier_series import RandomTruncatedFourierSeries
 __all__ = [
     "BaseIC",
     "BaseRandomICGenerator",
+    "ClampingICGenerator",
     "DiffusedNoise",
     "GaussianRandomField",
     "MultiChannelIC",
