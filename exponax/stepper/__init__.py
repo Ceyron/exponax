014883f0216d9d68e074193ff5eef6c80daf891a
Felix Köhler <felix.matteo@t-online.de>
2024-03-13 07:34:55 +0100

    Fix missing KdV stepper

--- exponax/stepper/__init__.py
+++ exponax/stepper/__init__.py
@@ -2,6 +2,7 @@ from ._burgers import Burgers
 from ._convection import GeneralConvectionStepper
 from ._general_nonlinear import GeneralNonlinearStepper1d
 from ._gradient_norm import GeneralGradientNormStepper
+from ._korteweg_de_vries import KortewegDeVries
 from ._kuramoto_sivashinsky import KuramotoSivashinsky, KuramotoSivashinskyConservative
 from ._linear import (
     Advection,
@@ -22,6 +23,7 @@ __all__ = [
     "Dispersion",
     "HyperDiffusion",
     "Burgers",
+    "KortewegDeVries",
     "KuramotoSivashinsky",
     "KuramotoSivashinskyConservative",
     "GeneralPolynomialStepper",
