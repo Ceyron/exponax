Add more option to fix the standard deviation

exponax/ic/_diffused_noise.py
@@ -12,6 +12,7 @@ class DiffusedNoise(BaseRandomICGenerator):
     domain_extent: float
     intensity: float
     zero_mean: bool
+    std_one: bool
     max_one: bool
 
     def __init__(
@@ -21,6 +22,7 @@ class DiffusedNoise(BaseRandomICGenerator):
         domain_extent: float = 1.0,
         intensity=0.001,
         zero_mean: bool = True,
+        std_one: bool = False,
         max_one: bool = False,
     ):
         """
@@ -28,7 +30,8 @@ class DiffusedNoise(BaseRandomICGenerator):
         field.
 
         The original noise is drawn in state space with a uniform normal
-        distribution.
+        distribution. After the application of the diffusion operator, the
+        spectrum decays exponentially with a rate of `intensity`.
 
         **Arguments**:
             - `num_spatial_dims`: The number of spatial dimensions `d`.
@@ -38,13 +41,20 @@ class DiffusedNoise(BaseRandomICGenerator):
             - `intensity`: The intensity of the noise. Defaults to `0.001`.
             - `zero_mean`: Whether to zero the mean of the noise. Defaults to
                 `True`.
+            - `std_one`: Whether to normalize the noise to have a standard
+                deviation of one. Defaults to `False`.
             - `max_one`: Whether to normalize the noise to the maximum absolute
                 value of one. Defaults to `False`.
         """
+        if not zero_mean and std_one:
+            raise ValueError("Cannot have `zero_mean=False` and `std_one=True`.")
+        if std_one and max_one:
+            raise ValueError("Cannot have `std_one=True` and `max_one=True`.")
         self.num_spatial_dims = num_spatial_dims
         self.domain_extent = domain_extent
         self.intensity = intensity
         self.zero_mean = zero_mean
+        self.std_one = std_one
         self.max_one = max_one
 
     def __call__(
@@ -64,6 +74,9 @@ class DiffusedNoise(BaseRandomICGenerator):
 
         if self.zero_mean:
             ic = ic - jnp.mean(ic)
+
+        if self.std_one:
+            ic = ic / jnp.std(ic)
 
         if self.max_one:
             ic = ic / jnp.max(jnp.abs(ic))