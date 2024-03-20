df8d228fee26df59ceb721d1fb8a20ef8916f7fa
Felix Koehler <f.koehler@tum.de>
2024-03-20 09:31:33 +0100

    Add Gradient Norm docstring

--- exponax/nonlin_fun/_gradient_norm.py
+++ exponax/nonlin_fun/_gradient_norm.py
@@ -21,7 +21,43 @@ class GradientNormNonlinearFun(BaseNonlinearFun):
         scale: float = 1.0,
     ):
         """
-        Uses by default a scaling of 0.5 to take into account the conservative evaluation
+        Performs a pseudo-spectral evaluation of the nonlinear gradient norm,
+        e.g., found in the Kuramoto-Sivashinsky equation in combustion format.
+        In 1d and state space, this reads
+
+        ```
+            𝒩(u) = b₂ 1/2 (u²)ₓ
+        ```
+
+        with a scale `b₂`. In higher dimensions, u has to be single channel and
+        the nonlinear function reads
+
+        ```
+            𝒩(u) = b₂ 1/2 ‖∇u‖₂²
+        ```
+
+        with `‖∇u‖₂²` the squared L2 norm of the gradient of `u`.
+
+        **Arguments:**
+            - `num_spatial_dims`: The number of spatial dimensions `d`.
+            - `num_points`: The number of points `N` used to discretize the
+                domain. This **includes** the left boundary point and
+                **excludes** the right boundary point. In higher dimensions; the
+                number of points in each dimension is the same.
+            - `derivative_operator`: A complex array of shape `(d, ..., N//2+1)`
+                that represents the derivative operator in Fourier space.
+            - `dealiasing_fraction`: The fraction of the highest resolved modes
+                that are not aliased. Defaults to `2/3` which corresponds to
+                Orszag's 2/3 rule.
+            - `zero_mode_fix`: Whether to set the zero mode to zero. In other
+                words, whether to have mean zero energy after nonlinear function
+                activation. This exists because the nonlinear operation happens
+                after the derivative operator is applied. Naturally, the
+                derivative sets any constant offset to zero. However, the square
+                nonlinearity introduces again a new constant offset. Setting
+                this argument to `True` removes this offset. Defaults to `True`.
+            - `scale`: The scale `b₂` of the gradient norm term. Defaults to
+              `1.0`.
         """
         super().__init__(
             num_spatial_dims,
