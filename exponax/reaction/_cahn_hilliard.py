044d944f7e20f5326f8d2f36e83d9554455ed9eb
Felix Koehler <f.koehler@tum.de>
2024-03-21 12:30:05 +0100

    Add docs

--- exponax/reaction/_cahn_hilliard.py
+++ exponax/reaction/_cahn_hilliard.py
@@ -61,6 +61,60 @@ class CahnHilliard(BaseStepper):
         num_circle_points: int = 16,
         circle_radius: float = 1.0,
     ):
+        """
+        Timestepper for the d-dimensional (`d ∈ {1, 2, 3}`) Cahn-Hilliard
+        reaction-diffusion equation on periodic boundary conditions. This model
+        is related to the Allen-Cahn equation. In 1d, it reads
+
+        ```
+            uₜ = ν ∂ₓₓ (c₃ u³ + c₁ u − γ uₓₓ)
+        ```
+
+        with `ν` the diffusivity, `c₁` the first order coefficient, `c₃` the
+        third order coefficient, and `γ` the gamma parameter. The state always
+        only has one channel. In higher dimensions, the equation reads
+
+        ```
+            uₜ = ν Δ (c₃ u³ + c₁ u - γ Δu)
+        ```
+
+        Since the Laplace operator is self-multiplied, there will be spatial
+        mixing.
+
+        **Arguments:**
+            - `num_spatial_dims`: The number of spatial dimensions `d`.
+            - `domain_extent`: The size of the domain `L`; in higher dimensions
+                the domain is assumed to be a scaled hypercube `Ω = (0, L)ᵈ`.
+            - `num_points`: The number of points `N` used to discretize the
+                domain. This **includes** the left boundary point and
+                **excludes** the right boundary point. In higher dimensions; the
+                number of points in each dimension is the same. Hence, the total
+                number of degrees of freedom is `Nᵈ`.
+            - `dt`: The timestep size `Δt` between two consecutive states.
+            - `diffusivity`: The diffusivity `ν`.
+            - `gamma`: The gamma parameter `γ`.
+            - `first_order_coefficient`: The first order coefficient `c₁`.
+            - `third_order_coefficient`: The third order coefficient `c₃`.
+            - `dealiasing_fraction`: The fraction of the highest wavenumbers to
+                dealias. Default is `1/2` because the default polynomial has a
+                highest degree of 3.
+            - `order`: The order of the Exponential Time Differencing Runge
+                Kutta method. Must be one of {0, 1, 2, 3, 4}. The option `0`
+                only solves the linear part of the equation. Use higher values
+                for higher accuracy and stability. The default choice of `2` is
+                a good compromise for single precision floats.
+            - `num_circle_points`: How many points to use in the complex contour
+                integral method to compute the coefficients of the exponential
+                time differencing Runge Kutta method. Default: 16.
+            - `circle_radius`: The radius of the contour used to compute the
+                coefficients of the exponential time differencing Runge Kutta
+                method. Default: 1.0.
+
+        **Notes:**
+            - See
+                https://github.com/chebfun/chebfun/blob/db207bc9f48278ca4def15bf90591bfa44d0801d/spin.m#L89
+                for an example IC of the Cahn-Hilliard in 1d.
+        """
         self.diffusivity = diffusivity
         self.gamma = gamma
         self.first_order_coefficient = first_order_coefficient
