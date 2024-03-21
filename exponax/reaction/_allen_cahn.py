ff60be3fe6f9b6783e9e32833a6da79c5939d01f
Felix Koehler <f.koehler@tum.de>
2024-03-21 09:51:44 +0100

    Restructure Allen-Cahn

--- exponax/reaction/_allen_cahn.py
+++ exponax/reaction/_allen_cahn.py
@@ -6,6 +6,9 @@ from ..nonlin_fun import PolynomialNonlinearFun
 
 
 class AllenCahn(BaseStepper):
+    diffusivity: float
+    first_order_coefficient: float
+    third_order_coefficient: float
     dealiasing_fraction: float
 
     def __init__(
@@ -15,12 +18,18 @@ class AllenCahn(BaseStepper):
         num_points: int,
         dt: float,
         *,
+        diffusivity: float = 5e-3,
+        first_order_coefficient: float = 1.0,
+        third_order_coefficient: float = -1.0,
         order: int = 2,
-        dealiasing_fraction: float = 1
-        / 2,  # Needs lower value due to cubic nonlinearity
+        # Needs lower value due to cubic nonlinearity
+        dealiasing_fraction: float = 1 / 2,
         num_circle_points: int = 16,
         circle_radius: float = 1.0,
     ):
+        self.diffusivity = diffusivity
+        self.first_order_coefficient = first_order_coefficient
+        self.third_order_coefficient = third_order_coefficient
         self.dealiasing_fraction = dealiasing_fraction
         super().__init__(
             num_spatial_dims=num_spatial_dims,
@@ -38,7 +47,7 @@ class AllenCahn(BaseStepper):
         derivative_operator: Complex[Array, "D ... (N//2)+1"],
     ) -> Complex[Array, "1 ... (N//2)+1"]:
         laplace = build_laplace_operator(derivative_operator, order=2)
-        linear_operator = laplace + 1.0
+        linear_operator = self.diffusivity * laplace + self.first_order_coefficient
         return linear_operator
 
     def _build_nonlinear_fun(
@@ -49,5 +58,5 @@ class AllenCahn(BaseStepper):
             self.num_spatial_dims,
             self.num_points,
             dealiasing_fraction=self.dealiasing_fraction,
-            coefficients=[0.0, 0.0, 0.0, -1.0],
+            coefficients=[0.0, 0.0, 0.0, self.third_order_coefficient],
         )
