f0f1e809d15816f43e88c7b4a012c722ce3ef9c6
Felix Koehler <f.koehler@tum.de>
2024-04-09 16:00:09 +0200

    Add test-based validation of Poisson

--- tests/test_validation.py
+++ tests/test_validation.py
@@ -66,6 +66,87 @@ def test_diffusion_1d():
     assert u_1_pred == pytest.approx(u_1, abs=1e-5)
 
 
+def test_validation_poisson_1d():
+    DOMAIN_EXTENT = 1.0
+    NUM_POINTS = 50
+
+    grid = ex.make_grid(1, DOMAIN_EXTENT, NUM_POINTS)
+
+    rhs = jnp.sin(2 * jnp.pi / DOMAIN_EXTENT * grid)
+    analytical_solution = (
+        jnp.sin(2 * jnp.pi / DOMAIN_EXTENT * grid) / (2 * jnp.pi / DOMAIN_EXTENT) ** 2
+    )
+
+    poisson_solver = ex.poisson.Poisson(
+        1,
+        DOMAIN_EXTENT,
+        NUM_POINTS,
+    )
+
+    u = poisson_solver(rhs)
+
+    assert u == pytest.approx(analytical_solution, abs=1e-6)
+
+
+def test_validation_poisson_2d():
+    DOMAIN_EXTENT = 3.0
+    NUM_POINTS = 25
+
+    grid = ex.make_grid(2, DOMAIN_EXTENT, NUM_POINTS)
+
+    rhs = jnp.sin(2 * jnp.pi / DOMAIN_EXTENT * grid[0:1]) * jnp.sin(
+        2 * jnp.pi / DOMAIN_EXTENT * grid[1:2]
+    )
+    analytical_solution = (
+        1
+        / 2
+        * jnp.sin(2 * jnp.pi / DOMAIN_EXTENT * grid[0:1])
+        * jnp.sin(2 * jnp.pi / DOMAIN_EXTENT * grid[1:2])
+        / (2 * jnp.pi / DOMAIN_EXTENT) ** 2
+    )
+
+    poisson_solver = ex.poisson.Poisson(
+        2,
+        DOMAIN_EXTENT,
+        NUM_POINTS,
+    )
+
+    u = poisson_solver(rhs)
+
+    assert u == pytest.approx(analytical_solution, abs=1e-6)
+
+
+def test_validation_poisson_3d():
+    DOMAIN_EXTENT = 3.0
+    NUM_POINTS = 15
+
+    grid = ex.make_grid(3, DOMAIN_EXTENT, NUM_POINTS)
+
+    rhs = (
+        jnp.sin(2 * jnp.pi / DOMAIN_EXTENT * grid[0:1])
+        * jnp.sin(2 * jnp.pi / DOMAIN_EXTENT * grid[1:2])
+        * jnp.sin(2 * jnp.pi / DOMAIN_EXTENT * grid[2:3])
+    )
+    analytical_solution = (
+        1
+        / 3
+        * jnp.sin(2 * jnp.pi / DOMAIN_EXTENT * grid[0:1])
+        * jnp.sin(2 * jnp.pi / DOMAIN_EXTENT * grid[1:2])
+        * jnp.sin(2 * jnp.pi / DOMAIN_EXTENT * grid[2:3])
+        / (2 * jnp.pi / DOMAIN_EXTENT) ** 2
+    )
+
+    poisson_solver = ex.poisson.Poisson(
+        3,
+        DOMAIN_EXTENT,
+        NUM_POINTS,
+    )
+
+    u = poisson_solver(rhs)
+
+    assert u == pytest.approx(analytical_solution, abs=1e-6)
+
+
 # Nonlinear steppers
 
 # Burgers can be test by comparing it with the solution obtained by Cole-Hopf
