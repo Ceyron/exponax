d3c0c5f60252bec26aa8d6a7ebb897517af61b65
Felix Köhler <27728103+Ceyron@users.noreply.github.com>
2024-09-18 15:47:58 +0200

    Add documentation

--- exponax/metrics/_fourier.py
+++ exponax/metrics/_fourier.py
@@ -25,7 +25,45 @@ def fourier_aggregator(
     derivative_order: Optional[float] = None,
 ) -> float:
     """
-    Sums up the gradient contirbutions
+    Aggregate over the spatial axes of a (channel-less) state array in Fourier
+    space.
+
+    While conceptually similar to [`exponax.metrics.spatial_aggregator`][], this
+    function additionally allows filtering specific frequency ranges and to take
+    derivatives. In higher dimensions, the derivative contributions (i.e., the
+    entries of the gradient) are summed up.
+
+    !!! info
+        The result of this function (under default settings) is (up to rounding
+        errors) identical to [`exponax.metrics.spatial_aggregator`][] for
+        `inner_exponent=1.0`. As such, it can be a consistent counterpart for
+        metrics based on the `L²(Ω)` functional norm.
+
+    !!! tip
+        To apply this function to a state tensor with a leading channel axis,
+        use `jax.vmap`.
+
+    **Arguments**:
+
+    - `state_no_channel`: The state tensor **without a leading channel
+        dimension**.
+    - `num_spatial_dims`: The number of spatial dimensions. If not specified,
+        it is inferred from the number of axes in `state_no_channel`.
+    - `domain_extent`: The extent `L` of the domain `Ω = (0, L)ᴰ`.
+    - `num_points`: The number of points `N` in each spatial dimension. If not
+        specified, it is inferred from the last axis of `state_no_channel`.
+    - `inner_exponent`: The exponent `p` each magnitude of a Fourier coefficient
+        is raised to before aggregation.
+    - `outer_exponent`: The exponent `q` the aggregated magnitudes are raised
+        to. If not specified, it is set to `1/p`.
+    - `low`: The lower cutoff (inclusive) frequency for filtering. If not
+        specified, it is set to `0`, meaning start it starts (including) the
+        mean/zero mode.
+    - `high`: The upper cutoff (inclusive) frequency for filtering. If not
+        specified, it is set to `N//2 + 1`, meaning it ends (including) at the
+        Nyquist mode.
+    - `derivative_order`: The order of the derivative to take. If not specified,
+        no derivative is taken.
     """
     if num_spatial_dims is None:
         num_spatial_dims = state_no_channel.ndim
@@ -114,7 +152,50 @@ def fourier_norm(
     high: Optional[int] = None,
     derivative_order: Optional[float] = None,
 ) -> float:
-    """Under normalized mode each channel is normalized separately. The channel is summed"""
+    """
+    Compute norms of states via aggregation in Fourier space.
+
+    Each channel is treated separately and the results are summed up.
+
+    While conceptually similar to [`exponax.metrics.spatial_norm`][], this
+    function additionally allows filtering specific frequency ranges and to take
+    derivatives. In higher dimensions, the derivative contributions (i.e., the
+    entries of the gradient) are summed up.
+
+    !!! tip
+        To operate on states with a leading batch axis, use `jax.vmap`. Then the
+        batch axis can be reduced, e.g., by `jnp.mean`. As a helper for this,
+        [`exponax.metrics.mean_metric`][] is provided.
+
+    If both `low` and `high` are `None`, the full spectrum is considered. In
+    this case, this function with `inner_exponent=2.0` (up to rounding errors)
+    produces the same result as [`exponax.metrics.spatial_norm`][] which is a
+    consequence of Parseval's theorem.
+
+
+    **Arguments**:
+
+    - `state`: The state tensor. Must follow the `Exponax` convention with a
+        leading channel axis, and either one, two, or three subsequent spatial
+        axes.
+    - `state_ref`: The reference state tensor. Must have the same shape as
+        `state`. If not specified, only the absolute norm of `state` is
+        computed.
+    - `mode`: The mode of the norm. Either `"absolute"` or `"normalized"`.
+    - `domain_extent`: The extent `L` of the domain `Ω = (0, L)ᴰ`.
+    - `inner_exponent`: The exponent `p` each magnitude of a Fourier coefficient
+        is raised to before aggregation.
+    - `outer_exponent`: The exponent `q` the aggregated magnitudes are raised
+        to. If not specified, it is set to `1/p`.
+    - `low`: The lower cutoff (inclusive) frequency for filtering. If not
+        specified, it is set to `0`, meaning start it starts (including) the
+        mean/zero mode.
+    - `high`: The upper cutoff (inclusive) frequency for filtering. If not
+        specified, it is set to `N//2 + 1`, meaning it ends (including) at the
+        Nyquist mode.
+    - `derivative_order`: The order of the derivative to take. If not specified,
+        no derivative is taken.
+    """
     if state_ref is None:
         if mode == "normalized":
             raise ValueError("mode 'normalized' requires state_ref")
@@ -163,6 +244,43 @@ def fourier_MAE(
     high: Optional[int] = None,
     derivative_order: Optional[float] = None,
 ) -> float:
+    """
+    Compute the mean absolute error in Fourier space.
+
+        ∑_(channels) ∑_(modi) (L/N)ᴰ |fft(uₕ - uₕʳ)|
+
+    The channel axis is summed **after** the aggregation.
+
+    While conceptually similar to [`exponax.metrics.spatial_MAE`][], this
+    function is **not** consistent with the `L¹(Ω)` functional norm. However, it
+    additionally allows filtering specific frequency ranges and to take
+    derivatives. In higher dimensions, the derivative contributions (i.e., the
+    entries of the gradient) are summed up.
+
+    !!! tip
+        To apply this function to a state tensor with a leading batch axis, use
+        `jax.vmap`. Then the batch axis can be reduced, e.g., by `jnp.mean`. As
+        a helper for this, [`exponax.metrics.mean_metric`][] is provided.
+
+
+    **Arguments**:
+
+    - `u_pred`: The state array. Must follow the `Exponax` convention
+        with a leading channel axis, and either one, two, or three subsequent
+        spatial axes.
+    - `u_ref`: The reference state array. Must have the same shape as
+        `u_pred`. If not specified, the MAE is computed against zero, i.e., the
+        norm of `u_pred`.
+    - `domain_extent`: The extent `L` of the domain `Ω = (0, L)ᴰ`.
+    - `low`: The lower cutoff (inclusive) frequency for filtering. If not
+        specified, it is set to `0`, meaning start it starts (including) the
+        mean/zero mode.
+    - `high`: The upper cutoff (inclusive) frequency for filtering. If not
+        specified, it is set to `N//2 + 1`, meaning it ends (including) at the
+        Nyquist mode.
+    - `derivative_order`: The order of the derivative to take. If not specified,
+        no derivative is taken.
+    """
     return fourier_norm(
         u_pred,
         u_ref,
@@ -185,6 +303,43 @@ def fourier_nMAE(
     high: Optional[int] = None,
     derivative_order: Optional[float] = None,
 ) -> float:
+    """
+    Compute the normalized mean absolute error in Fourier space.
+
+        ∑_(channels) (∑_(modi) (L/N)ᴰ |fft(uₕ - uₕʳ)| / ∑_(modi) (L/N)ᴰ
+        |fft(uₕʳ)|)
+
+    The channel axis is summed **after** the aggregation.
+
+    While conceptually similar to [`exponax.metrics.spatial_nMAE`][], this
+    function is **not** consistent with the `L¹(Ω)` functional norm. However, it
+    additionally allows filtering specific frequency ranges and to take
+    derivatives. In higher dimensions, the derivative contributions (i.e., the
+    entries of the gradient) are summed up.
+
+    !!! tip
+        To apply this function to a state tensor with a leading batch axis, use
+        `jax.vmap`. Then the batch axis can be reduced, e.g., by `jnp.mean`. As
+        a helper for this, [`exponax.metrics.mean_metric`][] is provided.
+
+
+    **Arguments**:
+
+    - `u_pred`: The state array. Must follow the `Exponax` convention with a
+        leading channel axis, and either one, two, or three subsequent spatial
+        axes.
+    - `u_ref`: The reference state array. Must have the same shape as
+        `u_pred`.
+    - `domain_extent`: The extent `L` of the domain `Ω = (0, L)ᴰ`.
+    - `low`: The lower cutoff (inclusive) frequency for filtering. If not
+        specified, it is set to `0`, meaning start it starts (including) the
+        mean/zero mode.
+    - `high`: The upper cutoff (inclusive) frequency for filtering. If not
+        specified, it is set to `N//2 + 1`, meaning it ends (including) at the
+        Nyquist mode.
+    - `derivative_order`: The order of the derivative to take. If not specified,
+        no derivative is taken.
+    """
     return fourier_norm(
         u_pred,
         u_ref,
@@ -207,6 +362,44 @@ def fourier_MSE(
     high: Optional[int] = None,
     derivative_order: Optional[float] = None,
 ) -> float:
+    """
+    Compute the mean squared error in Fourier space.
+
+        ∑_(channels) ∑_(modi) (L/N)ᴰ |fft(uₕ - uₕʳ)|²
+
+    The channel axis is summed **after** the aggregation.
+
+    Under default settings with correctly specific `domain_extent`, this
+    function (up to rounding errors) produces the identical result as
+    [`exponax.metrics.MSE`][] which is a consequence of Parseval's theorem.
+    However, it additionally allows filtering specific frequency ranges and to
+    take derivatives. In higher dimensions, the derivative contributions (i.e.,
+    the entries of the gradient) are summed up.
+
+    !!! tip
+        To apply this function to a state tensor with a leading batch axis, use
+        `jax.vmap`. Then the batch axis can be reduced, e.g., by `jnp.mean`. As
+        a helper for this, [`exponax.metrics.mean_metric`][] is provided.
+
+
+    **Arguments**:
+
+    - `u_pred`: The state array. Must follow the `Exponax` convention with a
+        leading channel axis, and either one, two, or three subsequent spatial
+        axes.
+    - `u_ref`: The reference state array. Must have the same shape as
+        `u_pred`. If not specified, the MSE is computed against zero, i.e., the
+        norm of `u_pred`.
+    - `domain_extent`: The extent `L` of the domain `Ω = (0, L)ᴰ`.
+    - `low`: The lower cutoff (inclusive) frequency for filtering. If not
+        specified, it is set to `0`, meaning start it starts (including) the
+        mean/zero mode.
+    - `high`: The upper cutoff (inclusive) frequency for filtering. If not
+        specified, it is set to `N//2 + 1`, meaning it ends (including) at the
+        Nyquist mode.
+    - `derivative_order`: The order of the derivative to take. If not specified,
+        no derivative is taken.
+    """
     return fourier_norm(
         u_pred,
         u_ref,
@@ -229,6 +422,42 @@ def fourier_nMSE(
     high: Optional[int] = None,
     derivative_order: Optional[float] = None,
 ) -> float:
+    """
+    Compute the normalized mean squared error in Fourier space.
+
+        ∑_(channels) (∑_(modi) (L/N)ᴰ |fft(uₕ - uₕʳ)|² / ∑_(modi) (L/N)ᴰ
+        |fft(uₕʳ)|²)
+
+    The channel axis is summed **after** the aggregation.
+
+    Under default settings with correctly specific `domain_extent`, this
+    function (up to rounding errors) produces the identical result as
+    [`exponax.metrics.nMSE`][] which is a consequence of Parseval's theorem.
+    However, it additionally allows filtering specific frequency ranges and to
+    take derivatives. In higher dimensions, the derivative contributions (i.e.,
+    the entries of the gradient) are summed up.
+
+    !!! tip
+        To apply this function to a state tensor with a leading batch axis, use
+        `jax.vmap`. Then the batch axis can be reduced, e.g., by `jnp.mean`. As
+        a helper for this, [`exponax.metrics.mean_metric`][] is provided.
+
+    **Arguments:**
+
+    - `u_pred`: The state array. Must follow the `Exponax` convention with a
+        leading channel axis, and either one, two, or three subsequent spatial
+        axes.
+    - `u_ref`: The reference state array. Must have the same shape as `u_pred`.
+    - `domain_extent`: The extent `L` of the domain `Ω = (0, L)ᴰ`.
+    - `low`: The lower cutoff (inclusive) frequency for filtering. If not
+        specified, it is set to `0`, meaning start it starts (including) the
+        mean/zero mode.
+    - `high`: The upper cutoff (inclusive) frequency for filtering. If not
+        specified, it is set to `N//2 + 1`, meaning it ends (including) at the
+        Nyquist mode.
+    - `derivative_order`: The order of the derivative to take. If not specified,
+        no derivative is taken.
+    """
     return fourier_norm(
         u_pred,
         u_ref,
@@ -251,6 +480,43 @@ def fourier_RMSE(
     high: Optional[int] = None,
     derivative_order: Optional[float] = None,
 ) -> float:
+    """
+    Compute the root mean squared error in Fourier space.
+
+        ∑_(channels) √(∑_(modi) (L/N)ᴰ |fft(uₕ - uₕʳ)|²)
+
+    The channel axis is summed **after** the aggregation.
+
+    Under default settings with correctly specific `domain_extent`, this
+    function (up to rounding errors) produces the identical result as
+    [`exponax.metrics.RMSE`][] which is a consequence of Parseval's theorem.
+    However, it additionally allows filtering specific frequency ranges and to
+    take derivatives. In higher dimensions, the derivative contributions (i.e.,
+    the entries of the gradient) are summed up.
+
+    !!! tip
+        To apply this function to a state tensor with a leading batch axis, use
+        `jax.vmap`. Then the batch axis can be reduced, e.g., by `jnp.mean`. As
+        a helper for this, [`exponax.metrics.mean_metric`][] is provided.
+
+    **Arguments**:
+
+    - `u_pred`: The state array. Must follow the `Exponax` convention with a
+        leading channel axis, and either one, two, or three subsequent spatial
+        axes.
+    - `u_ref`: The reference state array. Must have the same shape as `u_pred`.
+        If not specified, the RMSE is computed against zero, i.e., the norm of
+        `u_pred`.
+    - `domain_extent`: The extent `L` of the domain `Ω = (0, L)ᴰ`.
+    - `low`: The lower cutoff (inclusive) frequency for filtering. If not
+        specified, it is set to `0`, meaning start it starts (including) the
+        mean/zero mode.
+    - `high`: The upper cutoff (inclusive) frequency for filtering. If not
+        specified, it is set to `N//2 + 1`, meaning it ends (including) at the
+        Nyquist mode.
+    - `derivative_order`: The order of the derivative to take. If not specified,
+        no derivative is taken.
+    """
     return fourier_norm(
         u_pred,
         u_ref,
@@ -273,6 +539,42 @@ def fourier_nRMSE(
     high: Optional[int] = None,
     derivative_order: Optional[float] = None,
 ) -> float:
+    """
+    Compute the normalized root mean squared error in Fourier space.
+
+        ∑_(channels) (√(∑_(modi) (L/N)ᴰ |fft(uₕ - uₕʳ)|²) / √(∑_(modi) (L/N)ᴰ
+        |fft(uₕʳ)|²))
+
+    The channel axis is summed **after** the aggregation.
+
+    Under default settings with correctly specific `domain_extent`, this
+    function (up to rounding errors) produces the identical result as
+    [`exponax.metrics.nRMSE`][] which is a consequence of Parseval's theorem.
+    However, it additionally allows filtering specific frequency ranges and to
+    take derivatives. In higher dimensions, the derivative contributions (i.e.,
+    the entries of the gradient) are summed up.
+
+    !!! tip
+        To apply this function to a state tensor with a leading batch axis, use
+        `jax.vmap`. Then the batch axis can be reduced, e.g., by `jnp.mean`. As
+        a helper for this, [`exponax.metrics.mean_metric`][] is provided.
+
+    **Arguments**:
+
+    - `u_pred`: The state array. Must follow the `Exponax` convention with a
+        leading channel axis, and either one, two, or three subsequent spatial
+        axes.
+    - `u_ref`: The reference state array. Must have the same shape as `u_pred`.
+    - `domain_extent`: The extent `L` of the domain `Ω = (0, L)ᴰ`.
+    - `low`: The lower cutoff (inclusive) frequency for filtering. If not
+        specified, it is set to `0`, meaning start it starts (including) the
+        mean/zero mode.
+    - `high`: The upper cutoff (inclusive) frequency for filtering. If not
+        specified, it is set to `N//2 + 1`, meaning it ends (including) at the
+        Nyquist mode.
+    - `derivative_order`: The order of the derivative to take. If not specified,
+        no derivative is taken.
+    """
     return fourier_norm(
         u_pred,
         u_ref,
