e5a761565e57aef37544c81ef64afa534c05c731
Felix Koehler <f.koehler@tum.de>
2024-10-14 11:09:03 +0200

    Ensure mean is only taken over axis zero in case there are additional axes

--- exponax/metrics/_utils.py
+++ exponax/metrics/_utils.py
@@ -11,4 +11,5 @@ def mean_metric(
     'meanifies' a metric function to operate on arrays with a leading batch axis
     """
     wrapped_fn = lambda *a: metric_fn(*a, **kwargs)
-    return jnp.mean(jax.vmap(wrapped_fn)(*args))
+    metric_per_sample = jax.vmap(wrapped_fn, in_axes=0)(*args)
+    return jnp.mean(metric_per_sample, axis=0)
