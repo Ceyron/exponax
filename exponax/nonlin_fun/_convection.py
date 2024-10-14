488edcb650c85d4342d6ade6f0304a1cb5ce7440
Qiang <qiangliu.7@outlook.com>
2024-10-14 21:06:48 +0200

    add non-conservative form for convection operator

--- exponax/nonlin_fun/_convection.py
+++ exponax/nonlin_fun/_convection.py
@@ -8,6 +8,7 @@ class ConvectionNonlinearFun(BaseNonlinearFun):
     derivative_operator: Complex[Array, "D ... (N//2)+1"]
     scale: float
     single_channel: bool
+    conservative: bool
 
     def __init__(
         self,
@@ -18,23 +19,37 @@ class ConvectionNonlinearFun(BaseNonlinearFun):
         dealiasing_fraction: float = 2 / 3,
         scale: float = 1.0,
         single_channel: bool = False,
+        conservative: bool = False,
     ):
         """
         Performs a pseudo-spectral evaluation of the nonlinear convection, e.g.,
         found in the Burgers equation. In 1d and state space, this reads
 
         ```
-            𝒩(u) = b₁ 1/2 (u²)ₓ
+            𝒩(u) = b₁ u (u)ₓ
         ```
 
         with a scale `b₁`. The typical extension to higher dimensions requires u
         to have as many channels as spatial dimensions and then gives
 
         ```
-            𝒩(u) = b₁ 1/2 ∇ ⋅ (u ⊗ u)
+            𝒩(u) = b₁ u ⋅ ∇ u
+        ```
+        with `∇ ⋅` the divergence operator and the outer product `u ⊗ u`.
+
+        Meanwhile, if you use a conservative form, the convection term is given by
+
+        ```
+            𝒩(u) = b₁ u (u)ₓ
+        ```
+        for 1D and
+
+        ```
+            𝒩(u) = b₁ ∇ ⋅ (u ⊗ u)
         ```
 
-        with `∇ ⋅` the divergence operator and the outer product `u ⊗ u`.
+        for 2D and 3D.
+
         Another option is a "single-channel" hack requiring only one channel no
         matter the spatial dimensions. This reads
 
@@ -56,17 +71,19 @@ class ConvectionNonlinearFun(BaseNonlinearFun):
             - `scale`: The scale `b₁` of the convection term. Defaults to `1.0`.
             - `single_channel`: Whether to use the single-channel hack. Defaults
                 to `False`.
+            - `conservative`: Whether to use the conservative form. Defaults to `False`.
         """
         self.derivative_operator = derivative_operator
         self.scale = scale
         self.single_channel = single_channel
+        self.conservative=conservative
         super().__init__(
             num_spatial_dims,
             num_points,
             dealiasing_fraction=dealiasing_fraction,
         )
 
-    def _multi_channel_eval(
+    def _multi_channel_conservative_eval(
         self, u_hat: Complex[Array, "C ... (N//2)+1"]
     ) -> Complex[Array, "C ... (N//2)+1"]:
         num_channels = u_hat.shape[0]
@@ -78,12 +95,28 @@ class ConvectionNonlinearFun(BaseNonlinearFun):
         u = self.ifft(u_hat_dealiased)
         u_outer_product = u[:, None] * u[None, :]
         u_outer_product_hat = self.fft(u_outer_product)
-        convection = 0.5 * jnp.sum(
+        convection = jnp.sum(
             self.derivative_operator[None, :] * u_outer_product_hat,
             axis=1,
         )
         # Requires minus to move term to the rhs
         return -self.scale * convection
+    
+    def _multi_channel_nonconservative_eval(
+        self, u_hat: Complex[Array, "C ... (N//2)+1"]
+    ) -> Complex[Array, "C ... (N//2)+1"]:
+        num_channels = u_hat.shape[0]
+        if num_channels != self.num_spatial_dims:
+            raise ValueError(
+                "Number of channels in u_hat should match number of spatial dimensions"
+            )
+        u_hat_dealiased = self.dealias(u_hat)
+        u = self.ifft(u_hat_dealiased)
+        conv_u=sum(
+            [u[i:i+1]*self.ifft(self.derivative_operator[i:i+1]*u_hat) for i in range(num_channels)]
+        )
+        # Requires minus to move term to the rhs
+        return -self.scale * self.fft(conv_u)
 
     def _single_channel_eval(
         self, u_hat: Complex[Array, "C ... (N//2)+1"]
@@ -105,4 +138,7 @@ class ConvectionNonlinearFun(BaseNonlinearFun):
         if self.single_channel:
             return self._single_channel_eval(u_hat)
         else:
-            return self._multi_channel_eval(u_hat)
+            if self.conservative:
+                return self._multi_channel_conservative_eval(u_hat)
+            else:
+                return self._multi_channel_nonconservative_eval(u_hat)
