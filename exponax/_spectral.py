4ee241bb1a00ecd9782fe7a0347bae40543129cf
Felix Köhler <27728103+Ceyron@users.noreply.github.com>
2026-02-16 12:38:09 +0100

    Fix order zero case (#92)

--- exponax/_spectral.py
+++ exponax/_spectral.py
@@ -146,6 +146,12 @@ def build_laplace_operator(
     if order % 2 != 0:
         raise ValueError("Order must be even.")
 
+    if order == 0:
+        # Return the identity operator
+        return jnp.ones(
+            (1, *derivative_operator.shape[1:]), dtype=derivative_operator.dtype
+        )
+
     return jnp.sum(derivative_operator**order, axis=0, keepdims=True)
 
 
